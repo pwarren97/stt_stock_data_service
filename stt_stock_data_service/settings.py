ce4e26a399640c2d4022b0c3e4a98bdd10da4739
pwarren97 <pwarren97@gmail.com>
2021-01-17 17:27:19 -0600

    Updated requirements.txt; deciding whether or not to use rest framework

--- stt_stock_data_service/settings.py
+++ stt_stock_data_service/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom for REST API
+# Comes from https://microservicesdocumentation.readthedocs.io/en/latest/contributing/python.html
