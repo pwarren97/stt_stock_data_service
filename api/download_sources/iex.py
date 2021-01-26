c125b53c38e67c6f0d27747b7d246cef3b5ba4dc
pwarren97 <pwarren97@gmail.com>
2021-01-26 16:27:13 -0600

    Added onto new download_sources

--- api/download_sources/iex.py
+++ api/download_sources/iex.py
@@ -1,5 +1,6 @@
 from .template import Source
 from iexfinance.stocks import get_historical_data
+from .settings import IEX_TOKEN
 
 class Iex(Source):
     """
@@ -21,11 +22,37 @@ class Iex(Source):
         # Run error checking from Source class
         Source.get_historical_data(ticker_symbols, start, end, close_only)
 
+        if close_only:
+            stock_data = pd.DataFrame(columns=["symbol", "date", "close", "volume"])
+        else:
+            stock_data = pd.DataFrame(columns=["symbol", "date", "open", "high", "low", "close", "volume"])
+
         # if no end is specified
         if end == None:
             there_is_only_one_date_listed = True
         else:
             there_is_only_one_date_listed = False
 
-        for ticker_symbols in ticker_symbols:
-            pass
+        for ticker_symbol in ticker_symbols:
+            ticker_symbol = ticker_symbol.upper()
+
+            stock_data = download_then_add_data(stock_data, ticker_symbol, start, end, output_format='pandas', close_only=close_only)
+
+# Eliminate IEXCloud specific information for putting data in the database
+def restructure_df(data_frame, ticker_symbol, close_only):
+    data_frame['date'] = data_frame.index
+    data_frame.index.name = None
+    data_frame.index = range(len(data_frame))
+    data_frame['symbol'] = ticker_symbol
+
+    if close_only:
+        data_frame = data_frame[["symbol", "date", "close", "volume"]]
+    else:
+        data_frame = data_frame[["symbol", "date", "open", "high", "low", "close", "volume"]]
+    return data_frame
+
+# downloads data from IEX then adds it to the relevant pd.DataFrame
+def download_then_add_data(stock_df, ticker_symbol, start, end, close_only):
+    temp = get_historical_data(ticker_symbol, start, end, output_format='pandas', token=IEX_TOKEN, close_only=close_only)
+    temp = restructure_df(temp, ticker_symbol, close_only)
+    return pd.concat([stock_df, temp], ignore_index=True)
--- api/download_sources/settings.py
+++ api/download_sources/settings.py
@@ -1,3 +1,6 @@
 # Settings for the download sources
 
 DOWNLOAD_SOURCE = 'iex'
+
+# [ IEX ]
+IEX_TOKEN="pk_f48e8027956046a2a4ead5ef29a042e3"
