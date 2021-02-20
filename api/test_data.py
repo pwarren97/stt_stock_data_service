0c08512afa6e743570ee20788bf449b4b77310d9
pwarren97 <pwarren97@gmail.com>
2021-02-19 22:09:18 -0600

    All function arguments for source.get_historical_data are tested in api.tests.DownloadSourceTestCase.test_get_historical_data

--- api/test_data.py
+++ api/test_data.py
@@ -5,19 +5,20 @@ import numpy as np
 from datetime import datetime, timedelta
 
 start_date = datetime(2021, 1, 25)
-end_date = start_date + timedelta(days=2)
+end_date = start_date + timedelta(days=1)
 
 stock1 = 'AAPL'
 stock2 = 'TSLA'
+
+# stock2_close1 = 880.8
+# stock2_volume1 = 41173397
 
 date1 = str(start_date.date())
 date2 = str(end_date.date())
 # _columns = ['symbol', 'date', 'open', 'high', 'low', 'close', 'volume']
 # _columns_close_only = ['symbol', 'date', 'close', 'volume']
 
-
-# close_only = False
-correct_test1 = {
+_stock_data = {
     stock1: {
         date1: {
             "open": 143.07,
@@ -26,20 +27,189 @@ correct_test1 = {
             "close": 142.92,
             "volume": 157611713
         },
+        date2: {
+            "open": 143.6,
+            "high": 144.3,
+            "low": 141.37,
+            "close": 143.16,
+            "volume": 98390555
+        }
+    },
+    stock2: {
+        date1: {
+            "open": 855.0,
+            "high": 900.4,
+            "low": 838.8201,
+            "close": 880.8,
+            "volume": 41173397
+        },
+        date2: {
+            "open": 891.38,
+            "high": 895.9,
+            "low": 871.6,
+            "close": 883.09,
+            "volume": 23131603
+        }
+    }
+}
+
+# 1 Date
+# close_only = False
+correct_test1 = {
+    stock1: {
+        date1: {
+            "open":   _stock_data[ stock1 ][ date1 ][ 'open' ],
+            "high":   _stock_data[ stock1 ][ date1 ][ 'high' ],
+            "low":    _stock_data[ stock1 ][ date1 ][ 'low' ],
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
+        }
     }
 }
 # close_only = True
 correct_test2 = {
     stock1: {
         date1: {
-            "close": 142.92,
-            "volume": 157611713
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
         }
     }
 }
 
+# 2 dates
+# close_only = False
+correct_test3 = {
+    stock1: {
+        date1: {
+            "open":   _stock_data[ stock1 ][ date1 ][ 'open' ],
+            "high":   _stock_data[ stock1 ][ date1 ][ 'high' ],
+            "low":    _stock_data[ stock1 ][ date1 ][ 'low' ],
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
+        },
 
+        date2: {
+            "open":   _stock_data[ stock1 ][ date2 ][ 'open' ],
+            "high":   _stock_data[ stock1 ][ date2 ][ 'high' ],
+            "low":    _stock_data[ stock1 ][ date2 ][ 'low' ],
+            "close":  _stock_data[ stock1 ][ date2 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date2 ][ 'volume' ]
+        }
+    }
+}
 
+# close_only = True
+correct_test4 = {
+    stock1: {
+        date1: {
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
+        },
 
-# correct_test1 = pd.DataFrame(correct_test1, columns=_columns)
-# correct_test2 = pd.DataFrame(correct_test2, columns=_columns_close_only)
+        date2: {
+            "close":  _stock_data[ stock1 ][ date2 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date2 ][ 'volume' ]
+        }
+    }
+}
+
+# 2 stocks
+# 1 date
+# close_only = False
+correct_test5 = {
+    stock1: {
+        date1: {
+            "open":   _stock_data[ stock1 ][ date1 ][ 'open' ],
+            "high":   _stock_data[ stock1 ][ date1 ][ 'high' ],
+            "low":    _stock_data[ stock1 ][ date1 ][ 'low' ],
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
+        }
+    },
+    stock2: {
+        date1: {
+            "open":   _stock_data[ stock2 ][ date1 ][ 'open' ],
+            "high":   _stock_data[ stock2 ][ date1 ][ 'high' ],
+            "low":    _stock_data[ stock2 ][ date1 ][ 'low' ],
+            "close":  _stock_data[ stock2 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock2 ][ date1 ][ 'volume' ]
+        }
+    }
+}
+# close_only = True
+correct_test6 = {
+    stock1: {
+        date1: {
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
+        }
+    },
+    stock2: {
+        date1: {
+            "close":  _stock_data[ stock2 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock2 ][ date1 ][ 'volume' ]
+        }
+    }
+}
+
+# 2 stocks
+# 2 dates
+# close_only = False
+correct_test7 = {
+    stock1: {
+        date1: {
+            "open":   _stock_data[ stock1 ][ date1 ][ 'open' ],
+            "high":   _stock_data[ stock1 ][ date1 ][ 'high' ],
+            "low":    _stock_data[ stock1 ][ date1 ][ 'low' ],
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
+        },
+        date2: {
+            "open":   _stock_data[ stock1 ][ date2 ][ 'open' ],
+            "high":   _stock_data[ stock1 ][ date2 ][ 'high' ],
+            "low":    _stock_data[ stock1 ][ date2 ][ 'low' ],
+            "close":  _stock_data[ stock1 ][ date2 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date2 ][ 'volume' ]
+        }
+    },
+    stock2: {
+        date1: {
+            "open":   _stock_data[ stock2 ][ date1 ][ 'open' ],
+            "high":   _stock_data[ stock2 ][ date1 ][ 'high' ],
+            "low":    _stock_data[ stock2 ][ date1 ][ 'low' ],
+            "close":  _stock_data[ stock2 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock2 ][ date1 ][ 'volume' ]
+        },
+        date2: {
+            "open":   _stock_data[ stock2 ][ date2 ][ 'open' ],
+            "high":   _stock_data[ stock2 ][ date2 ][ 'high' ],
+            "low":    _stock_data[ stock2 ][ date2 ][ 'low' ],
+            "close":  _stock_data[ stock2 ][ date2 ][ 'close' ],
+            "volume": _stock_data[ stock2 ][ date2 ][ 'volume' ]
+        }
+    }
+}
+
+# close_only = True
+correct_test8 = {
+    stock1: {
+        date1: {
+            "close":  _stock_data[ stock1 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date1 ][ 'volume' ]
+        },
+        date2: {
+            "close":  _stock_data[ stock1 ][ date2 ][ 'close' ],
+            "volume": _stock_data[ stock1 ][ date2 ][ 'volume' ]
+        }
+    },
+    stock2: {
+        date1: {
+            "close":  _stock_data[ stock2 ][ date1 ][ 'close' ],
+            "volume": _stock_data[ stock2 ][ date1 ][ 'volume' ]
+        },
+        date2: {
+            "close":  _stock_data[ stock2 ][ date2 ][ 'close' ],
+            "volume": _stock_data[ stock2 ][ date2 ][ 'volume' ]
+        }
+    }
+}
--- api/tests.py
+++ api/tests.py
@@ -26,28 +26,31 @@ class DownloadSourceTestCase(TestCase):
 
         # 1 stock passed through
         test1 = source.get_historical_data([ test_data.stock1 ], test_data.start_date)
-        # test2 = source.get_historical_data([ test_data.stock1 ], test_data.start_date, close_only=True)
-        # test3 = source.get_historical_data([ test_data.stock1 ], test_data.start_date, test_data.end_date)
-        # test4 = source.get_historical_data([ test_data.stock1 ], test_data.start_date, test_data.end_date, close_only=True)
+        test2 = source.get_historical_data([ test_data.stock1 ], test_data.start_date, close_only=True)
+        test3 = source.get_historical_data([ test_data.stock1 ], test_data.start_date, test_data.end_date)
+        test4 = source.get_historical_data([ test_data.stock1 ], test_data.start_date, test_data.end_date, close_only=True)
         # # 2 stocks passed through
         #
-        # test5 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date)
-        # test6 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date, close_only=True)
-        # test7 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date, test_data.end_date)
-        # test8 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date, test_data.end_date, close_only=True)
+        test5 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date)
+        test6 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date, close_only=True)
+        test7 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date, test_data.end_date)
+        test8 = source.get_historical_data([ test_data.stock1, test_data.stock2 ], test_data.start_date, test_data.end_date, close_only=True)
         #
         #
         # # Tests
         self.assertEqual(test1, test_data.correct_test1)
-        # self.assertEqual(test2, test_data.correct_test2)
-        # self.assertEqual(test3, test_data.correct_test3)
-        # self.assertEqual(test4, test_data.correct_test4)
+        self.assertEqual(test2, test_data.correct_test2)
+        self.assertEqual(test3, test_data.correct_test3)
+        self.assertEqual(test4, test_data.correct_test4)
         #
-        # self.assertEqual(test5, test_data.correct_test5)
-        # self.assertEqual(test6, test_data.correct_test6)
-        # self.assertEqual(test7, test_data.correct_test7)
-        # self.assertEqual(test8, test_data.correct_test8)
+        self.assertEqual(test5, test_data.correct_test5)
+        self.assertEqual(test6, test_data.correct_test6)
+        self.assertEqual(test7, test_data.correct_test7)
+        self.assertEqual(test8, test_data.correct_test8)
 
 
     def test_get_ticker_symbols(self):
+        pass
+
+    def test_get_current_price(self):
         pass
